6e1bff8876b4b16db41769fdf92fe6cdae9f970a
lucasweidas <lucasweidas@gmail.com>
2023-07-15 11:15:36 -0300

    add NewsletterForm component

---
+++ components/newsletterForm.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { InferType, object, string } from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+
+const schema = object({
+  email: string().email('Please insert a valid email').required('Please insert an email'),
+});
+type FormData = InferType<typeof schema>;
+
+export function NewsletterForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  function onSubmit() {
+    reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex gap-2">
+        <div className="flex-1 relative">
+          <input
+            type="text"
+            id="email"
+            placeholder="Updates in your inbox…"
+            className={`text-sm text-blue-900 placeholder:text-gray-400 px-6 py-3 rounded-full w-full focus-visible:ring-2 outline-none ring-red-500 relative ${
+              errors.email ? 'focus-visible:ring-red-600' : ''
+            }`}
+            aria-label="Email"
+            {...register('email')}
+          />
+          {isSubmitSuccessful && (
+            <span className="bg-red-500 w-full h-full px-6 py-3 text-sm absolute inset-0 rounded-full text-center text-white font-bold clip-signed-up animate-signed-up">
+              Signed Up
+            </span>
+          )}
+          {errors.email && <p className="text-red-600 text-xs italic left-6 top-[calc(100%+0.5rem)] absolute">{errors.email.message}</p>}
+        </div>
+        <button
+          type="submit"
+          className="text-sm text-white bg-red-500 py-3 px-7 font-bold rounded-full hover:brightness-125 hover:grayscale-20 focus-visible:brightness-125 focus-visible:grayscale-20 transition-filter shrink-0 self-start"
+        >
+          Go
+        </button>
+      </div>
+    </form>
+  );
+}
