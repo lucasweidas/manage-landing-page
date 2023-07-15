84f5862e32f5d6ab2e4d608128b0d046d79d8b54
lucasweidas <lucasweidas@gmail.com>
2023-07-15 16:20:57 -0300

    add hover and focus-visible effect

--- components/header.tsx
+++ components/header.tsx
@@ -37,7 +37,11 @@ function Nav() {
           </svg>
         </a>
         {device === 'mobile' && !isOpen && (
-          <button className="text-blue-900" aria-label="Open navigation menu" onClick={toggleNav}>
+          <button
+            className="text-blue-900 hover:text-red-500 focus-visible:text-red-500 transition-colors"
+            aria-label="Open navigation menu"
+            onClick={toggleNav}
+          >
             <svg className="w-6.1 h-4.5" aria-hidden="true">
               <use xlinkHref="#hamburger-icon" />
             </svg>
@@ -47,7 +51,11 @@ function Nav() {
       {(isOpen || device === 'desktop') && (
         <div className="fixed bg-overlay w-full top-0 left-0 min-h-screen px-6 pt-10 gap-10 flex flex-col z-50" onClick={handleOverlayClick}>
           {device === 'mobile' && (
-            <button className="text-blue-900 self-end" aria-label="Close navigation menu" onClick={toggleNav}>
+            <button
+              className="text-blue-900 self-end hover:text-red-500 focus-visible:text-red-500 transition-colors"
+              aria-label="Close navigation menu"
+              onClick={toggleNav}
+            >
               <svg className="w-5.1 h-5.5" aria-hidden="true">
                 <use xlinkHref="#close-icon" />
               </svg>
@@ -55,19 +63,29 @@ function Nav() {
           )}
           <ul className="bg-white flex flex-col gap-7 p-10 items-center rounded-md text-blue-900 font-bold">
             <li>
-              <a href="#">Pricing</a>
+              <a href="#" className="hover:text-gray-400 focus-visible:text-gray-400 transition-colors">
+                Pricing
+              </a>
             </li>
             <li>
-              <a href="#">Product</a>
+              <a href="#" className="hover:text-gray-400 focus-visible:text-gray-400 transition-colors">
+                Product
+              </a>
             </li>
             <li>
-              <a href="#">About Us</a>
+              <a href="#" className="hover:text-gray-400 focus-visible:text-gray-400 transition-colors">
+                About Us
+              </a>
             </li>
             <li>
-              <a href="#">Careers</a>
+              <a href="#" className="hover:text-gray-400 focus-visible:text-gray-400 transition-colors">
+                Careers
+              </a>
             </li>
             <li>
-              <a href="#">Community</a>
+              <a href="#" className="hover:text-gray-400 focus-visible:text-gray-400 transition-colors">
+                Community
+              </a>
             </li>
           </ul>
           {device === 'desktop' && <a href="#">Get Started</a>}
